8d260d614bf1ed723362806b8382cc826740f56d
oakfire <oakwatcher@gmail.com>
2014-05-21 03:09:20 +0000

    0.0.1

---
+++ oak-app.js
@@ -0,0 +1,12 @@
+var logger = require('./lib/logger')('oak-app');
+var config = require('./lib/config');
+
+logger.info('oak-app started, pid: ' + process.pid);
+
+var http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Oak-app\n');
+}).listen(config.default_port);
+
+logger.info('oak-app running at port: '+config.default_port);
