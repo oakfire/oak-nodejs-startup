7cc354f32c768e0f0da6e2cd8ba0837aa98ecbca
oakfire <oakwatcher@gmail.com>
2014-07-09 06:06:16 +0000

    add comment to config file;log level config

---
+++ config/default.js
@@ -0,0 +1,9 @@
+/** if changed conifg, restart process to enable new config */
+module.exports = {    
+    /** default port */
+    "default_port": 3000,
+    /** silent console or not */
+    "console_quiet": false,
+    /** log level, can be "silly" "debug" "verbose" "info" "warn" "error" */
+    "log_level": "debug"
+}
